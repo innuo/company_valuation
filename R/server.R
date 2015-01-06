8c858e09a874d5b32a54d1928700edc6effb80bf
KR <kripashankar@gmail.com>
2015-01-06 10:36:20 -0600

    Updated tabbed panels using rCharts

--- R/server.R
+++ R/server.R
@@ -1,4 +1,5 @@
-library(shiny)
+require(shiny)
+require(rCharts)
 source("learn_predict.R")
 
 models <- list(readRDS("../data/model_ebidta.Rdata"), readRDS("../data/model_revenues.Rdata"),
@@ -9,36 +10,46 @@ model.choices <- c("Multiple of EBIDTA", "Multiple of Revenue", "Statistical (no
 
 shinyServer(
   function(input, output) {
-     output$text1 <- renderText({ 
-      paste("You have selected", input$id)
-    })
+      output$text1 <- renderText({ 
+        id <- input$id
+        test.index <- which(test.data$ids == id)
+        
+        model.id <- input$model.id
+        model <- models[[which(model.choices == model.id)]]
+        
+        result <- predict(model, test.index)
+        paste(
+              "Predicted value $", round(result$mean.prediction,0), 
+              " versus actual value of $", round(result$value,0)
+        )
+      })
     
-    output$table <- renderTable({
-      id <- input$id
-      test.index <- which(test.data$ids == id)
-      
-      model.id <- input$model.id
-      model <- models[[which(model.choices == model.id)]]
-      
-      result <- predict(model, test.index)
-      
-    #  if(result$)
-      
-      dat <- cbind.data.frame(Name=result$neighbor.ids, 
-                              Similarity = result$neighbor.similarities,
-                              result$neighbor.features.na)
-      dat <- dat[order(dat$Similarity, decreasing=TRUE),]
-      dat
-    })
+      #output$table <- renderTable({
+      output$table <- renderChart2({
+        id <- input$id
+        test.index <- which(test.data$ids == id)
     
-    output$table_test <- renderTable({
-      id <- input$id
-      test.index <- which(test.data$ids == id)
+        model.id <- input$model.id
+        model <- models[[which(model.choices == model.id)]]
       
-      dat <- cbind.data.frame(Name=test.data$ids[test.index], test.data$data[test.index,])
-      dat
-    })
+        result <- predict(model, test.index)
+      
+        #  if(result$)
+        dat <- cbind.data.frame(Name=result$neighbor.ids, 
+                                Similarity = result$neighbor.similarities,
+                                result$neighbor.features.na)
+        dat <- dat[order(dat$Similarity, decreasing=TRUE),]
+        #dat
+        dTable(dat, sPaginationType = "two_button")
+      })
     
-    
+      output$table_test <- renderTable({
+        id <- input$id
+        test.index <- which(test.data$ids == id)
+      
+        dat <- cbind.data.frame(Name=test.data$ids[test.index], test.data$data[test.index,])
+        dat
+      })
+      
   }
 )
--- R/ui.R
+++ R/ui.R
@@ -1,22 +1,50 @@
-library(shiny)
+require(shiny)
+require(rCharts)
 test.data <- readRDS("../data/company_test_data.Rdata")
 model.choices <- c("Multiple of EBIDTA", "Multiple of Revenue", "Statistical (no industry group)")
 
 
-shinyUI(fluidPage(
-  titlePanel("Beyond Compare: Company Valuation Demo"),
-  sidebarLayout(sidebarPanel(
-    selectizeInput(
-      'id', 'Test Company', choices = test.data$ids, multiple = FALSE
-    ),
-    selectizeInput(
-      'model.id', 'Model',
-      choices = model.choices
-    )),
+shinyUI(
+  fluidPage(
+    titlePanel("Beyond Compare: Company Valuation Demo"),
+    sidebarLayout(
+      sidebarPanel(
+        selectizeInput(
+          'id', 'Test Company', choices = test.data$ids, multiple = FALSE
+        ),
+        selectizeInput(
+          'model.id', 'Model',
+          choices = model.choices
+        )
+      ),
+    
     mainPanel(
-      textOutput("text1"),
-      tableOutput("table_test"),
-      tableOutput("table")
+      tabsetPanel(
+        tabPanel("Input", tableOutput("table_test")), 
+        tabPanel("Valuation", textOutput("text1")), 
+        tabPanel("Comps", showOutput("table", "datatables"))
+      )
     )
   )
 ))
+
+# Previous non-tabbed version
+# shinyUI(
+#   fluidPage(
+#     titlePanel("Beyond Compare: Company Valuation Demo"),
+#     sidebarLayout(sidebarPanel(
+#       selectizeInput(
+#         'id', 'Test Company', choices = test.data$ids, multiple = FALSE
+#       ),
+#       selectizeInput(
+#         'model.id', 'Model',
+#         choices = model.choices
+#       )
+#     ),
+#     mainPanel(
+#       textOutput("text1"),
+#       tableOutput("table_test"),
+#       tableOutput("table")
+#     )
+#   )
+# ))
